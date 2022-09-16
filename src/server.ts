0a80fa468422ed0a1e00298a0c61b7582ae3fb47
Tobi Olanrewaju <olantobi@gmail.com>
2022-09-16 13:41:52 +0100

    Add axios to download image

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import Jimp from 'jimp';
 
 import { IndexRouter } from './controllers/v0/index.router';
 import { V0MODELS } from './controllers/v0/model.index';
 
 import { sequelize } from './sequelize';
 
+import {filterImageFromURL, deleteLocalFiles, validateUrl } from './util/util';
+
 const app = express();
 const PORT = process.env.PORT || 9000;
 
-sequelize.addModels(V0MODELS);
-sequelize.sync().then(() => console.log('connected to database'));
+// sequelize.addModels(V0MODELS);
+// sequelize.sync().then(() => console.log('connected to database'));
 
 app.use(express.json());
 
@@ -19,6 +22,43 @@ app.use(express.json());
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
+
+app.get('/filteredimage', async (req: Request, res: Response) => {
+  const image_url = req.query.image_url as string;
+
+  if (!image_url) {
+    return res.status(400).send({message: 'No image url provided'});
+  }
+
+  if (!validateUrl(image_url)) {
+    return res.status(422).send({message: 'Invalid image url provided'});
+  }
+
+  let filteredImage;
+  try {
+    filteredImage = await filterImageFromURL(image_url);
+  } catch(err) {
+    let message;
+    if (err instanceof Error) message = err.message;
+    else message = String(err)
+
+    return res.status(422).send({message: message });
+  }
+
+  res.send({data: filteredImage});
+
+  deleteLocalFiles([filteredImage]);
+})
+
+app.get('/test', (req, res) => {
+  Jimp.read("https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg")
+  .then(image => {
+
+        console.log(image.bitmap);
+        res.end(image.bitmap);
+    });
+});
+
 
 app.use('/api/v0', IndexRouter);
 
